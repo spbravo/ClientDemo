0e3ee3abc98998373412d0ee70025ce02d677f78
spbravo <spbravo@gmail.com>
2020-10-02 11:39:38 +0200

    Update AemetClient.java

--- ClientDemo/src/main/java/com/example/resilience/AemetClient.java
+++ ClientDemo/src/main/java/com/example/resilience/AemetClient.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface AemetClient {
 		@RequestMapping(method = RequestMethod.GET, value = "/opendata/api/prediccion/especifica/monta%C3%B1a/pasada/area/peu1", consumes = "application/json")
 		//String consulta(@RequestParam(value="api_key", defaultValue="peu1") String area);
-	    String consulta(@RequestParam(value="api_key", defaultValue="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzcGJyYXZvQGdtYWlsLmNvbSIsImp0aSI6IjgyNGM1Y2UzLWFmNzYtNDk0NS1hZDBmLTdhMDk1ZTIyMzJkZCIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNTA5NTU2OTY5LCJ1c2VySWQiOiI4MjRjNWNlMy1hZjc2LTQ5NDUtYWQwZi03YTA5NWUyMjMyZGQiLCJyb2xlIjoiIn0.XAEUT7p_9sXrkavMunL9CtQySwUWOicCIbGfsYxdVZk") String apiKey);
+	    String consulta(@RequestParam(value="api_key", defaultValue="VySWQiOiI4MjRjNWNlMy1hZjc2LTQ5NDUtYWQwZi03YTA5NWUyMjMyZGQiLCJyb2xlIjoiIn0.XAEUT7p_9sXrkavMunL9CtQySwUWOicCIbGfsYxdVZk") String apiKey);
 
 }
